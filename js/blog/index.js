378f0b7a232e45935ed1d1939b62fedd260b3324
ismumu <ismmm@qq.com>
2018-03-22 17:05:59 +0800

    add

--- js/blog/index.js
+++ js/blog/index.js
@@ -1,3 +1,15 @@
 $(function () {
-    $(".post-list").pin({containerSelector: ".sidebar"})
+    $(".post-list").pin({ containerSelector: ".sidebar" })
+
+    var gitment = new Gitment({
+        // id: '页面 ID', // 可选。默认为 location.href
+        owner: 'ismumu',
+        repo: 'ismumu-comments',
+        oauth: {
+            client_id: '967be3ff5cbadbc11347',
+            client_secret: '88dca966f3d89abafe950ff037ef2c7a21538941',
+        },
+    })
+    gitment.render('comments');
+
 })
